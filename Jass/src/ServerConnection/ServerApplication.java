8b603c3ed1ea9e188a790d7a13b9094ebd71712a
Domi89 <48253327+Domi89@users.noreply.github.com>
2020-03-09 13:47:14 +0100

    server

---
+++ Jass/src/ServerConnection/ServerApplication.java
@@ -0,0 +1,33 @@
+package ServerConnection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerApplication {
+
+	public static void main (String[] args) {
+		try(ServerSocket serverSocket = new ServerSocket(45136)){
+			
+			Socket socket = serverSocket.accept();
+			System.out.println("Client connected");
+			BufferedReader input = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+			
+			while(true) {
+				String echoString = input.readLine();
+				if(echoString.equals("exit")) {
+					break;
+				}
+				output.println("Echo from Server "+echoString);
+			}
+			
+		} catch(IOException e){
+			System.out.println("Serverexception: "+e.getMessage());			
+		}
+	}
+}
