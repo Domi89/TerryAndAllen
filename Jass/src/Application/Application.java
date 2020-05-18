c963d0c4ab5b820094c6da2b3a5f45d26bcd0093
dhaef <dhaef@DESKTOP-I8TPQLU.home>
2020-05-18 21:03:30 +0200

    update

--- Jass/src/Application/Application.java
+++ Jass/src/Application/Application.java
@@ -2,6 +2,9 @@ package Application;
 
 import BusinessLayer.JassServerModel;
 
+//since it is JavaFX SDK 14, Run as -> VM arguments module path add controls and javafx.fxml
+//additionally there must be added the JAVA FX 14 Library
+
 public class Application {
 	
 	public static void main(String[] args) {
