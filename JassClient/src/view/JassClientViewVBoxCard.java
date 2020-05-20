dcae50b7e22193721e9ca26eb3e827a62a2421f9
NordstromN <niklas.nordstroem@students.fhnw.ch>
2020-05-20 19:32:24 +0200

    krasse for int methode geschrieben #hackertime

--- JassClient/src/view/JassClientViewVBoxCard.java
+++ JassClient/src/view/JassClientViewVBoxCard.java
@@ -1,8 +1,5 @@
 package view;
 
-import java.io.FileInputStream;
-
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
@@ -14,45 +11,16 @@ public class JassClientViewVBoxCard extends VBox{
 		
 		for (int i = 0; i < 4; i++) {
 			String cardNr = "imageUrl"+i;
-			createCard(cardNr);		
+			createCard(cardNr);	
+			}
 		}
-		
-		
-	/*	String imageUrl1 = "/images/backOfCard.jpg";
-		String imageUrl2 = "/images/backOfCard.jpg";
-		String imageUrl3 = "/images/backOfCard.jpg";
-		String imageUrl4 = "/images/backOfCard.jpg";
-		
-		Image image1 = new Image(imageUrl1, 100, 150, false, true);
-		Image image2 = new Image(imageUrl2, 100, 150, false, true);
-		Image image3 = new Image(imageUrl3, 100, 150, false, true);
-		Image image4 = new Image(imageUrl4, 100, 150, false, true);
-		
-        ImageView imageView1 = new ImageView(image1);	
-        ImageView imageView2 = new ImageView(image2);
-        ImageView imageView3 = new ImageView(image3);
-        ImageView imageView4 = new ImageView(image4);
-         */
-		
-        this.getChildren().addAll(imageView);
-       
-		
-		//this.getChildren().add(test);	
-		
-		
-		
-	}
 
-	private createCard(String cardNr) {
-		cardNr = "/images/backOfCard.jpg";
-		Image image = new Image(cardNr, 100, 150, false, true); 
-		ImageView imageView = new ImageView(image);	
-	} 
-	return imageView
-	
-	
-	
-	
+		private void createCard(String cardNr) {
+			cardNr = "/images/backOfCard.jpg";
+			Image image = new Image(cardNr, 100, 150, false, true); 
+			ImageView imageView = new ImageView(image);	
+			this.getChildren().addAll(imageView);
+		} 
 	
 
 }
