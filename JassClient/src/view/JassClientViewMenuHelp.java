1234f3fe4b1f0c0f9ce6d6b8da642258e29609bb
NordstromN <niklas.nordstroem@students.fhnw.ch>
2020-05-23 20:19:25 +0200

    asdf

--- JassClient/src/view/JassClientViewMenuHelp.java
+++ JassClient/src/view/JassClientViewMenuHelp.java
@@ -1,13 +1,15 @@
 package view;
 
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class JassClientViewMenuHelp {
+	
+		Stage settingsStage;
 	
 		
 	public static void helpMenu() {
@@ -17,43 +19,45 @@ public class JassClientViewMenuHelp {
 	
 		settingsStage.initModality(Modality.APPLICATION_MODAL);
 		settingsStage.setTitle("Jass Regeln");
-		settingsStage.setMinWidth(250);
 
-		
-		Button close = new Button("Ah Ok, Schliessen!");
 		
 		
 		Label topLabel = new Label("Hier findest du die Jass-Regeln:");
 		topLabel.setStyle(
 				"-fx-font-size: 15px ; "
 				+ "-fx-font-weight: bold;"
-				+"-fx-background-color: lightgrey;"		
-				+ " -fx-text-fill: Black");
+				+ " -fx-text-fill: white");
 		
+		Hyperlink link = new Hyperlink();
+		link.setText("https://lmgtfy.com/?q=Jass+Regeln+einfach");
+		link.setStyle("-fx-font-size: 15px;"  
+				+	"-fx-text-fill: white; ");
+		
+		
+		
+		/*
 		Label linkLabel = new Label("https://lmgtfy.com/?q=Jass+Regeln+einfach" );
 		linkLabel.setStyle(
 				"-fx-font-size: 15px; "
-				+"-fx-background-color: lightgrey;"
-				+ " -fx-text-fill: black");
+				
+				+ " -fx-text-fill: white");
+		*/
+		
 		
 		VBox vBox = new VBox();
-		vBox.getChildren().addAll(topLabel, linkLabel, close);
+		vBox.getChildren().addAll(topLabel, link);
 		vBox.setStyle("-fx-min-width: 400;"
-		+"-fx-min-height: 200;"
+		+"-fx-min-height: 150;"
 		+"-fx-max-width: 400;"
-		+"-fx-max-height: 200;"
+		+"-fx-max-height: 150;"
 		+"-fx-background-image: url(/images/backgroundLogin.jpg)");
 		Scene scene = new Scene (vBox);
 	
 		settingsStage.setScene(scene);
 		settingsStage.showAndWait();
 		
-		close.setOnAction(e->{
-			settingsStage.close();
-		});
+		}
 
-		
+			
 	}
-	
 
-}
