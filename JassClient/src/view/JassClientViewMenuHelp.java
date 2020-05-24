47f6b7792e9ed57d7e8b1fdbcdb84590f9b68e29
NordstromN <niklas.nordstroem@students.fhnw.ch>
2020-05-24 12:03:55 +0200

    test

--- JassClient/src/view/JassClientViewMenuHelp.java
+++ JassClient/src/view/JassClientViewMenuHelp.java
@@ -2,6 +2,7 @@ package view;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
@@ -26,18 +27,16 @@ public class JassClientViewMenuHelp {
 		Label topLabel = new Label("Hier findest du die Jass-Regeln:");
 		topLabel.setStyle(
 				"-fx-font-size: 15px ; "
-				+ "-fx-font-weight: bold;"
-				+"-fx-background-color: lightgrey;"		
-				+ " -fx-text-fill: Black");
+			+ " -fx-text-fill: White");
 		
-		Label linkLabel = new Label("https://lmgtfy.com/?q=Jass+Regeln+einfach" );
-		linkLabel.setStyle(
-				"-fx-font-size: 15px; "
-				+"-fx-background-color: lightgrey;"
-				+ " -fx-text-fill: black");
-		
+		Hyperlink link = new Hyperlink();
+				link.setText("https://lmgtfy.com/?q=Jass+Regeln+einfach");
+				link.setStyle("-fx-font-size: 15px;"  
+						+	"-fx-text-fill: white; ");
+
+			
 		VBox vBox = new VBox();
-		vBox.getChildren().addAll(topLabel, linkLabel, close);
+		vBox.getChildren().addAll(topLabel, link, close);
 		vBox.setStyle("-fx-min-width: 400;"
 		+"-fx-min-height: 200;"
 		+"-fx-max-width: 400;"
@@ -45,15 +44,20 @@ public class JassClientViewMenuHelp {
 		+"-fx-background-image: url(/images/backgroundLogin.jpg)");
 		Scene scene = new Scene (vBox);
 	
+		close.setOnAction(e -> {
+			settingsStage.close();
+		});
+		
+		
 		settingsStage.setScene(scene);
 		settingsStage.showAndWait();
 		
-		close.setOnAction(e->{
-			settingsStage.close();
-		});
 
 		
-	}
-	
+		};
 
-}
+	
+		
+		
+	}
+
